bb0bba6e097a25010247526e04db98171c6f3a04
orion55 <grol55@mail.ru>
2017-04-18 22:27:12 +0500

    formatXlsx

--- src/models/excel.js
+++ src/models/excel.js
@@ -24,6 +24,10 @@ export default class xlsxModel {
                     const range = Sheet.range(startCell, endCell);
                     range.value(rows);
 
+                    if (process.env.NODE_ENV === 'home')
+                        this.formatXlsx(workbook, headers.length, rows.length);
+
+
                     this.saveXlsx(workbook)
                         .then(fileUrl => resolve({status: 200, fileUrl: fileUrl}))
                 })
@@ -31,6 +35,38 @@ export default class xlsxModel {
                     reject({status: 500, msg: "Error export to Excel", detail_msg: err.message})
                 )
         })
+    };
+
+    formatXlsx = (workbook, maxHeaders, maxRows) => {
+        const Sheet = workbook.sheet(0);
+        let Range = Sheet.range(1, 1, 1, maxHeaders);
+        Range.style("fontColor", "ffffff").style("fill", "4F81BD").style("bold", true);
+
+        for (let i = 2; i <= maxRows + 1; i++) {
+            Range = Sheet.range(i, 1, i, maxHeaders);
+            if (i % 2) {
+                Range
+                    .style("fill", "FFFFFF")
+                    .style("fontColor", "000000")
+                    .style("bottomBorder", true)
+                    .style("borderColor", "95B3D7");
+            } else {
+                Range
+                    .style("fill", "DCE6F1")
+                    .style("fontColor", "000000")
+                    .style("bottomBorder", true)
+                    .style("borderColor", "95B3D7");
+            }
+        }
+
+        Range = Sheet.range(2, 1, maxRows + 1, 1);
+        Range.style("numberFormat", "dd.mm.yyyy");
+        Range = Sheet.range(2, 2, maxRows + 1, 2);
+        Range.style("numberFormat", "dd.mm.yyyy");
+
+        for (let i = 1; i <= maxHeaders; i++) {
+            Sheet.column(i).width(12);
+        }
     };
 
     saveXlsx = (workbook) => {
